cf08111d4078a3a0daa53d60dec135fb13f8b97e
Brian Ketelsen <bketelsen@gmail.com>
2016-12-01 09:14:55 -0500

    first three classes done

---
+++ pointersreftypes/demos/passbyref/main.go
@@ -0,0 +1,24 @@
+package main
+
+// main is the entry point for the application.
+func main() {
+	// Declare variable of type int with a value of 10.
+	count := 10
+
+	// Display the "value of" and "address of" count.
+	println("Before:", count, &count)
+
+	// Pass the "address of" the variable count.
+	increment(&count)
+
+	println("After: ", count, &count)
+}
+
+// increment declares inc as a pointer variable whose value is
+// always an address and points to values of type int.
+func increment(inc *int) {
+	// Increment the value that the "pointer points to". (de-referencing)
+	*inc++
+	println("Inc:   ", *inc, &inc, inc)
+
+}
