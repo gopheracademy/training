8d1bd266dda143a63d406d5993238bdb3187256b
Brian Ketelsen <bketelsen@gmail.com>
2017-02-08 11:30:20 -0500

    better remark support

--- firstpr/module.md
+++ firstpr/module.md
@@ -1,3 +1,11 @@
+class: center, middle
+name: Slide Name
+video: 
+description: 
+level: {Beginner, Intermediate, Advanced}
+topic: {Go, Kubernetes, Docker, Web, Distributed Systems}
+
+# Module Title Here (this is the Title Page of Presentation)
 ---
 name: Slide Name
 video: 
--- grifts/module.go
+++ grifts/module.go
@@ -46,7 +46,15 @@ var _ = Add("module:new", func(c *Context) error {
 	return err
 })
 
-const slide = `---
+const slide = `class: center, middle
+name: Slide Name
+video: 
+description: 
+level: {Beginner, Intermediate, Advanced}
+topic: {Go, Kubernetes, Docker, Web, Distributed Systems}
+
+# Module Title Here (this is the Title Page of Presentation)
+
 name: Slide Name
 video: 
 description: 
